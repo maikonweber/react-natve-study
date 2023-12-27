d381ecb7268bfaffd039789750c3f57145a374f4
MaikonWeber <maikonweber@gmail.com>
2023-12-27 19:34:55 -0300

    Add use effect and states

--- App.js
+++ App.js
@@ -1,23 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View , TextInput, Button} from 'react-native';
-import Title from './src/components/Index';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import Title from "./src/components/Index";
+import { useState, useEffect } from "react";
 
 export default function App() {
-  const [heigth, setHeight] = useState(null)
-  const [width, setWidth] = useState(null)
-  const [textButton, setTextButton] = useState(null)
-  const [messageImc, setMessageImc] = useState(null)
-  const [imc, setImc] = useState(null)
-  
-  return (
+  const [heigth, setHeight] = useState(null);
+  const [width, setWidth] = useState(null);
+  const [textButton, setTextButton] = useState(null);
+  const [messageImc, setMessageImc] = useState(null);
+  const [imc, setImc] = useState(null);
 
+  useEffect(() => {
+    setImc(heigth / width);
+    messageImc("Seu Imc é ")
+  }, [textButton]);
+  return (
     <View style={styles.container}>
       <Text> Teste de IMC </Text>
       <StatusBar style="auto" />
-      <Title></Title>           
-      <TextInput placeholder='Ex Numeric' keyboardType='numeric'> Digite seu Tamanho </TextInput>
-      <TextInput placeholder='Ex Numeric' keyboardType='numeric' > Digite sua Altura </TextInput>
-      <Button title='Button'> Red </Button>
+      <Title></Title>
+      <TextInput placeholder="Ex Numeric" keyboardType="numeric">
+        {" "}
+        Digite seu Tamanho{" "}
+      </TextInput>
+      <TextInput placeholder="Ex Numeric" keyboardType="numeric">
+        {" "}
+        Digite sua Altura{" "}
+      </TextInput>
+      <Button title="Button"> Red </Button>
     </View>
   );
 }
@@ -25,8 +35,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffff',
-    alignItems: 'center',
-    justifyContent: 'center',
-   },
+    backgroundColor: "#ffff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
